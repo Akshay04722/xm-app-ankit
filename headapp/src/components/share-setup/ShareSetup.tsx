54f934fb78c9e1089a81b273cd9eeec4a7e4d6e2
dmakwana <dmakwana@toastmasters.org>
2026-06-26 18:30:06 +0530

    feat: add ShareSetup component and initialize development build chunks

--- headapp/src/components/share-setup/ShareSetup.tsx
+++ headapp/src/components/share-setup/ShareSetup.tsx
@@ -104,6 +104,7 @@ export const Default = (props: ShareSetupProps): JSX.Element => {
                     sizes="(max-width: 640px) 100vw, (max-width: 1200px) 50vw, 400px"
                     className="object-cover w-full h-full"
                     alt={((imageField?.value?.alt as string) || `Setup ${index + 1}`)}
+                    unoptimized={true}
                   />
                 </div>
               );
